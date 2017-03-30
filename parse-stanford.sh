0752a701cbf965a691db14e805cfca1aeb70dace
tomeken-yoshinaga <kenta-yoshinaga@aist.go.jp>
2017-03-30 14:25:11 +0900

    [add/update] Renamed parse.sh and add parsing script for Stanford parser.

---
+++ parse-stanford.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+if [ ! $# -ge 2 ]; then
+  echo Usage: `basename $0` 'input output'
+  echo
+  exit
+fi
+
+scriptdir=`dirname $0`
+
+java -cp "$scriptdir/*" jigg.pipeline.Pipeline \
+  -annotators "corenlp[tokenize,ssplit,pos,parse]" \
+  -corenlp.ssplit.eolonly "true" \
+  -outputFormat json \
+  -nThreads 50 \
+  -file $1 \
+  -output $2 \
+  > /dev/null 2>&1
+
+python $scriptdir/generate_berkeley_trees.py $2 --format "txt"
